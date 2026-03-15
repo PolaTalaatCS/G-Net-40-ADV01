c0f77d253106052efae2221ba0c7704c30b29126
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-03-15 20:47:22 +0200

    Question 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace G_Net_40_ADV01
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace G_Net_40_ADV01
 {
     internal class Program
     {
@@ -9,18 +11,32 @@
             //we use genrics because it make Type Safety and better	Performance make Codes reusable and maintainable
             #endregion
             #region Question 2
-        // public class container<T> {
-        //  private T item;
-       //public void Add(T value)
-    //    {
-    //        item = value;
-    //    }
- //         public T Get()
-    //    {
-    //        return item;
-    //    }
+            // public class container<T> {
+            //  private T item;
+            //public void Add(T value)
+            //    {
+            //        item = value;
+            //    }
+            //         public T Get()
+            //    {
+            //        return item;
+            //    }
+            #endregion
+            #region Question 3
+            //it is  Generic classes that can have multiple type parameters
+        //    public class pair <T1 , T2>
+        //{
+        //    public T1 Tfirst {  get; set; }
+        //    public T2 Tsecond { get; set; }
+
+        //    public Pair(TFirst first, TSecond second)
+        //    {
+        //        First = first;
+        //        Second = second;
+        //    }
+        //}
             #endregion
 
+        }
     }
-}
 }
