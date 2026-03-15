2b20180c3dcc15d26e4a54c53df99c30f19ae242
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-03-15 20:40:21 +0200

    Question 2

--- Program.cs
+++ Program.cs
@@ -8,7 +8,19 @@
             // generics allow you to define type-safe classes, interfaces, methods, and delegates without committing to a specifiec data type  until the code is used
             //we use genrics because it make Type Safety and better	Performance make Codes reusable and maintainable
             #endregion
-           
-        }
+            #region Question 2
+        // public class container<T> {
+        //  private T item;
+       //public void Add(T value)
+    //    {
+    //        item = value;
+    //    }
+ //         public T Get()
+    //    {
+    //        return item;
+    //    }
+            #endregion
+
     }
+}
 }
