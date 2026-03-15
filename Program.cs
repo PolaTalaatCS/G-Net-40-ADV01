089826cee10903fe1c34df6f835ebc125ff1c235
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-03-15 20:29:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace G_Net_40_ADV01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+            // generics allow you to define type-safe classes, interfaces, methods, and delegates without committing to a specific data type until the code is used
+            //we use genrics because it make Type Safety and better	Performance make Code Reuse 
+            #endregion
+
+        }
+    }
+}
