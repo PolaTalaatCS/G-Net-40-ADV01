5d8bfa1a15dc3d358b491d3bfaa604bb9872df00
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-03-15 20:34:56 +0200

    Question 1

--- Program.cs
+++ Program.cs
@@ -5,10 +5,10 @@
         static void Main(string[] args)
         {
             #region Question 1
-            // generics allow you to define type-safe classes, interfaces, methods, and delegates without committing to a specific data type until the code is used
-            //we use genrics because it make Type Safety and better	Performance make Code Reuse 
+            // generics allow you to define type-safe classes, interfaces, methods, and delegates without committing to a specifiec data type  until the code is used
+            //we use genrics because it make Type Safety and better	Performance make Codes reusable and maintainable
             #endregion
-
+           
         }
     }
 }
